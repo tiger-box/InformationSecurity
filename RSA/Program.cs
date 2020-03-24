6ab9cc690db7e06776deab00c993749d8342ed2b
tiger-box <n7garrus@yandex.ru>
2020-03-24 21:38:08 +0700

    Сделана лабораторная работа №3

---
+++ RSA/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace RSA
+{
+    class Program
+    {
+        private static Dictionary<char, byte> alphabet = new Dictionary<char, byte>();
+
+        static void Main(string[] args)
+        {
+            // Генерируем алфавит
+            for (int i = 0; i < 26; i++)
+            {
+                alphabet.Add(
+                    Convert.ToChar(i + Convert.ToInt32('a')),
+                    Convert.ToByte(10 + i));
+            }
+
+            alphabet.Add(' ', 66);
+
+            Console.WriteLine("Choice program mode: e - encrypt; d - decrypt; g - generate key; any key - quit");
+            var programMode = Console.ReadKey(true).KeyChar;
+            if (programMode == 'e' || programMode == 'у')
+                Encrypt();
+            else if (programMode == 'd' || programMode == 'в')
+                Decrypt();
+            else if (programMode == 'g' || programMode == 'п')
+                GenerateKey();
+
+            Console.WriteLine("Для продолжения нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+
+        private static void Encrypt()
+        {
+            RSA encoder = new RSA();
+
+            // Открываем файл публичного ключа
+            using (var publicKeyFile = new FileStream("PublicKey.txt", FileMode.Open))
+            {
+                if (publicKeyFile == null)
+                {
+                    Console.WriteLine("Ошибка. Файл ключа для шифрования не может быть открыт");
+                    return;
+                }
+
+                byte[] key = new byte[publicKeyFile.Length];
+                publicKeyFile.Read(key, 0, key.Length);
+                encoder.ExportPublicKey(Encoding.UTF8.GetString(key));
+            }
+
+            byte[] inputText;
+            byte[] inputTextCode;
+            byte[] outputText = null;
+
+            // Открываем файл InputText.txt
+            // Если такого нет, вводим текст с клавиатуры и сохраняем в данный файл
+            using (var inputFile = new FileStream("InputText.txt", FileMode.OpenOrCreate))
+            {
+                // Если открытый файл пустой
+                if (inputFile.Length == 0)
+                {
+                    // Вводим текст с клавиатуры
+                    Console.WriteLine("Enter text");
+                    inputText = Encoding.UTF8.GetBytes(Console.ReadLine().ToLower());
+                    inputFile.Write(inputText, 0, inputText.Length);
+                }
+                else
+                {
+                    // Считываем текст с файла
+                    var array = new byte[inputFile.Length];
+                    inputFile.Read(array, 0, array.Length);
+                    inputText = Encoding.UTF8.GetBytes(Encoding.UTF8.GetString(array).ToLower());
+                }
+            }
+
+            inputTextCode = new byte[inputText.Length];
+
+            for (int i = 0; i < inputTextCode.Length; i++)
+            {
+                inputTextCode[i] = alphabet[Convert.ToChar(inputText[i])];
+            }
+
+            outputText = encoder.Encrypt(inputText);
+
+            // Записываем зашифрованный текст в файл EnryptedText.txt
+            using (FileStream outputFile = new FileStream("EnryptedText.txt", FileMode.Create))
+            {
+                if (outputFile != null)
+                {
+                    outputFile.Write(outputText, 0, outputText.Length);
+                }
+            }
+        }
+
+        private static void Decrypt()
+        {
+            byte[] inputText;
+            byte[] outputText = null;
+
+            RSA encoder = new RSA();
+
+            // Открываем файл приватного ключа
+            using (var privateKeyFile = new FileStream("PrivateKey.txt", FileMode.Open))
+            {
+                if (privateKeyFile == null)
+                {
+                    Console.WriteLine("Ошибка. Файл ключа для дешифрования не может быть открыт");
+                    return;
+                }
+
+                byte[] key = new byte[privateKeyFile.Length];
+                privateKeyFile.Read(key, 0, key.Length);
+                encoder.ExportPrivateKey(Encoding.UTF8.GetString(key));
+            }
+
+            // Открываем файл с зашифрованным текстом EnryptedText.txt
+            using (FileStream inputFile = new FileStream("EnryptedText.txt", FileMode.Open))
+            {
+                // Если файл пустой, то выводим сообщение об ошибке и завершаем программу
+                if (inputFile.Length == 0)
+                {
+                    Console.WriteLine("Ощибка. Файл EnryptedText.txt пуст");
+                    return;
+                }
+
+                inputText = new byte[inputFile.Length];
+                inputFile.Read(inputText, 0, inputText.Length);
+            }
+
+            outputText = encoder.Decrypt(inputText);
+
+            // Запись результатов в файл DeryptedText.txt
+            using (FileStream outputFile = new FileStream("DeryptedText.txt", FileMode.Create))
+            {
+                outputFile.Write(outputText, 0, outputText.Length);
+            }
+        }
+
+        private static void GenerateKey()
+        {
+
+            RSA encoder = new RSA();
+            encoder.GenerateKey();
+
+            // Создаем файл публичного ключа
+            using (FileStream publicFileKey = new FileStream("PublicKey.txt", FileMode.Create)) //запись результата в  файл
+            {
+                if (publicFileKey != null)
+                {
+
+                    string publicKey = encoder.ImportPublicKey();
+                    byte[] array = Encoding.Default.GetBytes(publicKey);
+                    publicFileKey.Write(array, 0, array.Length);
+                    Console.WriteLine("Файл публичного ключа создан");
+                }
+            }
+
+            // Создаем файл приватного ключа
+            using (FileStream privateFileKey = new FileStream("PrivateKey.txt", FileMode.Create)) //запись результата в  файл
+            {
+                if (privateFileKey != null)
+                {
+
+                    string privateKey = encoder.ImportPrivateKey();
+                    byte[] array = Encoding.Default.GetBytes(privateKey);
+                    privateFileKey.Write(array, 0, array.Length);
+                    Console.WriteLine("Файл секретного ключа создан");
+                }
+            }
+        }
+    }
+}
---
+++ RSA/RSA.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RSA
+{
+    public class RSA
+    {
+        private UInt64 P;
+        private UInt64 Q;
+        private UInt64 fi;
+        private UInt64 n;
+        private UInt64 publicKey;
+        private UInt64 privateKey;
+
+        /// <summary>
+        /// Вввод параметров P Q вручную, с проверкой на простое число
+        /// </summary>
+        public void GenerateKey()
+        {
+            do
+            {
+                Console.Write("Введите простое число P:\t");
+                P = Convert.ToUInt64(Console.ReadLine());
+
+                if (!IsPrime(P))
+                {
+                    Console.WriteLine("Ошибка. Необходимо простое число P (которое делится только на 1 или само себя)");
+                }
+            } while (!IsPrime(P));
+
+            do
+            {
+                Console.Write("Введите простое число Q:\t");
+                Q = Convert.ToUInt64(Console.ReadLine());
+
+                if (!IsPrime(Q))
+                {
+                    Console.WriteLine("Ошибка. Необходимо простое число Q (которое делится только на 1 или само себя)");
+                }
+            } while (!IsPrime(Q));
+
+            Console.Write("Введите экспоненту e:\t\t");
+            publicKey = Convert.ToUInt64(Console.ReadLine());
+
+            // Модуль - произведение P*Q
+            n = P * Q;
+
+            // Функция эйлера φ(n) = (p−1)⋅(q−1)
+            fi = (P - 1) * (Q - 1);
+
+            // Генерация публичной экспоненты
+            GeneratePublicExp();
+            // Генерация секретной экспоненты
+            GenerateSecretExp();
+        }
+
+        public string ImportPublicKey()
+        {
+            return Convert.ToString(publicKey) + ";" + Convert.ToString(n);
+        }
+
+        public string ImportPrivateKey()
+        {
+            return Convert.ToString(privateKey) + ";" + Convert.ToString(n);
+        }
+
+        public void ExportPublicKey(string PublicKey)
+        {
+            publicKey = Convert.ToUInt64(PublicKey.Split(new char[] { ';' })[0]);
+            n = Convert.ToUInt64(PublicKey.Split(new char[] { ';' })[1]);
+        }
+
+        public void ExportPrivateKey(string PrivateKey)
+        {
+            privateKey = Convert.ToUInt64(PrivateKey.Split(new char[] { ';' })[0]);
+            n = Convert.ToUInt64(PrivateKey.Split(new char[] { ';' })[1]);
+        }
+
+        /// <summary>
+        /// Шифрование текста
+        /// </summary>
+        /// <param name="Bytes">Массив байтов для шифрования</param>
+        public byte[] Encrypt(byte []Bytes)
+        {
+            byte[] EncryptText = new byte[Bytes.Length];
+
+            for (int i = 0; i < Bytes.Length; i++)
+            {
+                EncryptText[i] = Convert.ToByte(EncryptFunction(EncryptText[i]));
+            }
+
+            return EncryptText;
+        }
+
+        /// <summary>
+        /// Функция шифрования RSA
+        /// </summary>
+        private UInt64 EncryptFunction(byte Byte)
+        {
+            UInt64 input = Convert.ToUInt64(Byte);
+            UInt64 result = 1;
+
+            for (UInt64 j = 0; j < publicKey; j++)
+            {
+                result = result * input;
+                result = result % n;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Рашифровка текста
+        /// </summary>
+        /// <param name="Bytes">Массив зашифрованных байтов</param>
+        public byte[] Decrypt(byte[] Bytes)
+        {
+            byte[] DecryptText = new byte[Bytes.Length];
+
+            for (int i = 0; i < Bytes.Length; i++)
+            {
+                DecryptText[i] = Convert.ToByte(DecryptFunction(DecryptText[i]));
+            }
+
+            return DecryptText;
+        }
+
+        /// <summary>
+        /// Функция расшифровки
+        /// </summary>
+        private UInt64 DecryptFunction(byte Byte)
+        {
+            UInt64 input = Convert.ToUInt64(Byte);
+            UInt64 result = 1;
+
+            for (UInt64 j = 0; j < privateKey; j++)
+            {
+                result = result * input;
+                result = result % n;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Проверка простого числа
+        /// </summary>
+        private bool IsPrime(UInt64 Number)
+        {
+            UInt64 N = Convert.ToUInt64(Math.Sqrt(Convert.ToDouble(Number)));
+            for (UInt64 i = 2; i < N; i++)
+            {
+                if (Number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Проверка двух чисел на взаимопростоту
+        /// </summary>
+        private bool IsPrime(UInt64 Number1, UInt64 Number2)
+        {
+            if ((Number2 % Number1) == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Генерация публичной экспоненты (e)
+        /// </summary>
+        /// <returns></returns>
+        private void GeneratePublicExp()
+        {
+            // Проверка экспоненты (e) со значением функции Эйлера (fi) на взаимнопростое
+            if (IsPrime(publicKey, fi))
+            {
+                return;
+            }
+            
+            Console.WriteLine("Экспонента и значение функции Эйлера не являетюся взаимопростыми числами.\n" +
+                                  "Сгенерирована новая экспонента");
+
+            // Генерируем новую экспоненту
+            while (!IsPrime(publicKey) || !IsPrime(publicKey, fi))
+            {
+                publicKey = Convert.ToUInt64(new Random().Next(2, Convert.ToInt32(fi) - 1));
+            }
+        }
+
+        /// <summary>
+        /// Генерация секретной экспоненты (d)
+        /// </summary>
+        private void GenerateSecretExp()
+        {
+            UInt64 k = 1;
+
+            // Вычисляется число d, удовлетворяющее сравнению: d * e ≡ 1 (mod φ(n))
+            while (true)
+            {
+                k = k + fi;
+
+                if (k % publicKey == 0)
+                {
+                    privateKey = (k / publicKey);
+                    return;
+                }
+            }
+        }
+    }
+}
